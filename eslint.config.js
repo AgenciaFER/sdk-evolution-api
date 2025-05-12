ae8248597c7fd3beceaa25642e1a8a84ff81ba11
AgenciaFER <95007420+AgenciaFER@users.noreply.github.com>
2025-05-12 18:22:08 -0300

    chore: adiciona configuração básica do ESLint para TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// ESLint configuration for a TypeScript project
+export default {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // Customize your rules here
+  },
+};
